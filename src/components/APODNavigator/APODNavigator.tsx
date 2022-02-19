310188042d187f14d3753dbe9523097db5e7640d
Leonel Aguirre <leonel.aguirre@hbr.org>
2022-02-18 20:13:05 -0600

    APODNavigator component added.

--- src/components/APODNavigator/APODNavigator.tsx
+++ src/components/APODNavigator/APODNavigator.tsx
@@ -1,7 +1,19 @@
 import "./APODNavigator.scss"
 
 const APODNavigator = () => {
-  return <h1>APOD Navigator</h1>
+  return (
+    <div className="apod-navigator">
+      <section className="apod-navigator__header">
+        <h1 className="apod-navigator__title">
+          <span className="apod-navigator__drop-cap">A</span>stronomy{" "}
+          <span className="apod-navigator__drop-cap">P</span>icture{" "}
+          <span className="apod-navigator__drop-cap">O</span>f the{" "}
+          <span className="apod-navigator__drop-cap">D</span>ay
+        </h1>
+        <p className="apod-navigator__subtitle">Navigator</p>
+      </section>
+    </div>
+  )
 }
 
 export default APODNavigator
