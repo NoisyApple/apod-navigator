390962773a8d207f523a09376f4febda931caaaa
Leonel Aguirre <leonel.aguirre@hbr.org>
2022-02-18 21:42:21 -0600

    useAPOD hook now supports single strings and arrays of strings

--- src/hooks/useAPOD.tsx
+++ src/hooks/useAPOD.tsx
@@ -3,15 +3,19 @@ import { useEffect, useState } from "react"
 const API_KEY = "2LzGTT0e1hgVZZxmMBHkzfVG0BTy0E5iOxBraZg9"
 const BASE_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=`
 
-const useAPOD = (date: string) => {
-  const [data, setData] = useState([])
+type useAPODOverload = {
+  (date: string): object
+  (dates: string[]): object
+}
+
+const useAPOD: useAPODOverload = (date: string | string[]) => {
+  const [data, setData] = useState<object>({})
 
   useEffect(() => {
-    const constructedUrl = BASE_URL + date
-
-    fetch(constructedUrl)
-      .then((response) => response.json())
-      .then((retrievedData) => setData(retrievedData))
+    let datesArray = typeof date === "string" ? [date] : date
+    Promise.all(
+      datesArray.map((d) => fetch(BASE_URL + d).then((r) => r.json()))
+    ).then((retrievedData) => setData({ data: retrievedData }))
   }, [])
 
   return data
